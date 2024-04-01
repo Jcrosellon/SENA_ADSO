329dc570b623b1beda84d94c26ed74e3c7b06503
Jcrosellon <119710206+Jcrosellon@users.noreply.github.com>
2024-03-31 20:18:40 -0500

    Corregir errores en el juego de memoria

---
+++ games/games_memory/assets/js/main.js
@@ -0,0 +1,12 @@
+var objClass;
+const contId="containerGame";
+const progress="progressbarId";
+const chronometer="chronometerId";
+const speed=100;
+const maxMilliseconds=2000;
+
+function setLevel(value){
+
+    objClass=new Game(contId,value,progress,chronometer,speed,maxMilliseconds);
+    
+}
